a741066eb541098a9a961a0fd155bf1d34740b9d
Jason Judge <jason.judge@consil.co.uk>
2018-03-01 00:08:31 +0000

    Perform the cardRerence fetch only for certain payment types.

--- src/Message/CompleteRequest.php
+++ src/Message/CompleteRequest.php
@@ -33,49 +33,58 @@ class CompleteRequest extends AbstractRequest implements NotificationInterface
     {
         $this->validateNotificationData($data);
 
-        $gcResultPayment = !empty($data['gcResultPayment']) ? $data['gcResultPayment'] : null;
-        $success = ($gcResultPayment == Gateway::RESULT_PAYMENT_SUCCESS);
+        $paymentType = $this->getPaymentType();
 
-        $transactionReference = !empty($data['gcReference']) ? $data['gcReference'] : null;
-        $pkn = !empty($data['gcPkn']) ? $data['gcPkn'] : null;
+        // The cardReference may be available for a handlful of payment types.
 
-        // Here, if we don't have the pkn details, then try to fetch them.
-        // These are the pseudo card details for saving and reusing.
-        // Basically, we want to suplement the data we have got, with data
-        // from an additional API call.
-        // The "createCard" parameter must be set to trigger this action.
+        if (
+            $paymentType === Gateway::PAYMENT_TYPE_CREDIT_CARD
+            || $paymentType === Gateway::PAYMENT_TYPE_DIRECTDEBIT
+        ) {
+            $gcResultPayment = !empty($data['gcResultPayment']) ? $data['gcResultPayment'] : null;
+            $success = ($gcResultPayment == Gateway::RESULT_PAYMENT_SUCCESS);
 
-        if ($this->getCreateCard() && $success && empty($pkn) && !empty($transactionReference)) {
-            // Create a new gateway since we don't have access to the current
-            // gateway object from here.
+            $transactionReference = !empty($data['gcReference']) ? $data['gcReference'] : null;
+            $pkn = !empty($data['gcPkn']) ? $data['gcPkn'] : null;
 
-            $gateway = \Omnipay\Omnipay::create('GiroCheckout');
-            $gateway->initialize($this->getParameters());
-            $getCardRequest = $gateway->getCard([
-                'transactionReference' => $data['gcReference'],
-            ]);
-            $response = $getCardRequest->send();
+            // Here, if we don't have the pkn details, then try to fetch them.
+            // These are the pseudo card details for saving and reusing.
+            // Basically, we want to suplement the data we have got, with data
+            // from an additional API call.
+            // The "createCard" parameter must be set to trigger this action.
 
-            $cardData = $response->getData();
+            if ($this->getCreateCard() && $success && empty($pkn) && !empty($transactionReference)) {
+                // Create a new gateway since we don't have access to the current
+                // gateway object from here.
 
-            // Map card fields to fields that the Payment Page already uses.
-            // If the original authorisation request was issued without the
-            // "createCard" option set, then these details will not be availeble.
+                $gateway = \Omnipay\Omnipay::create('GiroCheckout');
+                $gateway->initialize($this->getParameters());
+                $getCardRequest = $gateway->getCard([
+                    'transactionReference' => $data['gcReference'],
+                ]);
+                $response = $getCardRequest->send();
 
-            if (! empty($cardData['pkn'])) {
-                $data['gcPkn'] = $cardData['pkn'];
-            }
+                $cardData = $response->getData();
 
-            if (! empty($cardData['cardnumber'])) {
-                $data['gcCardnumber'] = $cardData['cardnumber'];
-            }
+                // Map card fields to fields that the Payment Page already uses.
+                // If the original authorisation request was issued without the
+                // "createCard" option set, then these details will not be availeble.
 
-            // This endpoint returns the expiry date in two parts.
-            // The Payment Page APi returns the expiry date as one string.
-            // We will normalise the two parts to the format of the one string.
+                if (! empty($cardData['pkn'])) {
+                    $data['gcPkn'] = $cardData['pkn'];
+                }
 
-            if (! empty($cardData['expireyear']) && ! empty($cardData['expiremonth'])) {
-                $data['gcCardExpDate'] = $cardData['expiremonth'] . '/' . $cardData['expireyear'];
+                if (! empty($cardData['cardnumber'])) {
+                    $data['gcCardnumber'] = $cardData['cardnumber'];
+                }
+
+                // This endpoint returns the expiry date in two parts.
+                // The Payment Page APi returns the expiry date as one string.
+                // We will normalise the two parts to the format of the one string.
+
+                if (! empty($cardData['expireyear']) && ! empty($cardData['expiremonth'])) {
+                    $data['gcCardExpDate'] = $cardData['expiremonth'] . '/' . $cardData['expireyear'];
+                }
             }
         }
 
